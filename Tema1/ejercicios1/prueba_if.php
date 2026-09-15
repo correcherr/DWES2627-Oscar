ba1053cca8294c72c4e029894942f03e706f6d87
oscar <correcherclase@gmail.com>
2026-09-15 19:25:52 +0200

    Creacion de css para los ejercicios y ejercicios 4 y 5

---
+++ Tema1/ejercicios1/prueba_if.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Cuarto ejercicio</h1>
+    <?php
+    $nota1 = 8.67;
+    $nota2 = 9.21;
+    $nota3 = 9.67;
+    ?>
+    <p>EJ 4: La nota mayor es de
+        <?php
+        echo $nota1 > $nota2 ? $nota1 : $nota2;
+        ?>
+    </p>
+    <p>EJ 5: La nota mayor es de
+        <?php
+        if ($nota1 > $nota2 && $nota1 > $nota3) {
+            echo $nota1;
+        } else if ($nota2 > $nota3 && $nota2 > $nota3) {
+            echo $nota2;
+        } else {
+            echo $nota3;
+        }
+        ?>
+    </p>
+
+</body>
+
+</html>
